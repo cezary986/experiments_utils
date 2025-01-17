c19ab5b8cddaee4c1ba176e1841dd7e4212a9bcb
cezary.maszczyk <cezary.maszczyk@emag.lukasiewicz.gov.pl>
2025-01-17 09:46:39 +0100

    remove useless files

--- build/lib/experiments_utils/__init__.py
+++
@@ -1,9 +0,0 @@
-""".. include:: ../docs_src/main.md
-"""
-from .experiment import experiment, Experiment
-from .step import step, Step
-from .events.event_types import *
-from .events.events import *
-from .utils import *
-from .state import *
-from .store import Store
